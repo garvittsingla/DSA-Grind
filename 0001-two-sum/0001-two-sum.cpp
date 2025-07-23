971c6dcbf0ffe997ff8b5d43659a3ac3e890a1cc
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-07-23 23:16:47 +0530

    0001-TWO-SUM Stats: Time: 3 ms (70.63%), Space: 15 MB (20.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,16 +3,16 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mpp;
         vector<int> ans;
-        for(int i= 0 ; i < nums.size() ; i++){
-            int number = target - nums[i];
-            if(mpp.find(number)!=mpp.end()){
-                ans.push_back(mpp[number]);
+        for(int i = 0 ; i < nums.size() ; i++){
+            int diff = target - nums[i];
+            if(mpp.find(diff)!=mpp.end()){
                 ans.push_back(i);
+                ans.push_back(mpp[diff]);
                 return ans;
             }else{
-                mpp[nums[i]]=i;
+                mpp[nums[i]] = i;
             }
         }
-        return ans;
+        return {-1,-1};
     }
 };
