4fac59de8eefa17af38027081e4af5729ba03a95
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-07-23 20:02:57 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 11.6 MB (48.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> hashset(3,0);
-        int n = nums.size();
-        for(int i = 0 ; i < nums.size();i++){
-            hashset[nums[i]] += 1;
+      int low = 0;
+      int mid = 0;
+      int high= nums.size() -1;
+
+      while(mid <= high){
+        if(nums[mid]==0){
+            swap(nums[mid],nums[low]);
+            low++;
+            mid++;
+        }else if(nums[mid]==1){
+            mid++;
+        }else{
+            swap(nums[mid],nums[high]);
+            high--;
         }
-        int index= 0 ;
-        for(int i = 0 ; i < hashset[0] ; i++){
-            nums[index]=0;
-            index++;
-        }
-        for(int i = 0 ; i < hashset[1] ; i++){
-            nums[index]=1;
-            index++;
-        }
-        for(int i = 0 ; i < hashset[2] ; i++){
-            nums[index]=2;
-            index++;
-        }
+      }
+        
     }
 };
