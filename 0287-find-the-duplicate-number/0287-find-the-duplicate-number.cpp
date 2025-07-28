e7121459d06ae524a1b993af2ff4a7b6fe92fef7
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-07-28 18:04:48 +0530

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 113 ms (9.49%), Space: 104.6 MB (5.13%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        vector<int> hashset(nums.size(),0);
-        for(int i = 0 ; i < nums.size() ; i++){
-            hashset[nums[i]]++;
+      unordered_map<int,int> mpp;
+      for(int i = 0 ; i < nums.size() ; i++){
+        mpp[nums[i]]++;
+      }
+      for(auto it:mpp){
+        if(it.second > 1){
+            return it.first;
         }
-        for(int i =1 ; i < nums.size() ; i++){
-            if(hashset[i]>1){
-                return i;
-            }
-        }
-        return -1;
+      }
+      return -1;
 
     }
 };
