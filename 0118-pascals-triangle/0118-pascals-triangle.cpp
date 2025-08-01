d406e2d35d16b58445fc2519f457c793878a740e
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-08-01 11:31:59 +0530

    0118-PASCALS-TRIANGLE Stats: Time: 0 ms (100%), Space: 9.6 MB (88.29%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,10 +5,10 @@ public:
         for(int i = 1; i <= numRows ; i++){
             vector<int> temp(i);
             for(int j = 0 ; j < i ; j++){
-                if(j==0 || j==i-1){
+                if(j==0||j==i-1){
                     temp[j] = 1;
                 }else{
-                    temp[j]=triangle[i-2][j-1]+triangle[i-2][j];
+                    temp[j] = triangle[i-2][j]+triangle[i-2][j-1];
                 }
             }
             triangle.push_back(temp);
