95b81201c86ad47fe46b68b0b6f7c0b09ae4cd05
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-08-08 22:43:29 +0530

    0069-SQRTX Stats: Time: 3 ms (18.67%), Space: 8.5 MB (50.79%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        int start = 1;
+        int end = x;    
+        int mid = start+ (end-start)/2;
+        int possible = x;
+        while(start <= end){
+            long long sqaure =(long long)mid*mid;
+
+            if(sqaure<=x){
+                possible = mid;
+                start = mid+1;
+            }else{
+                end = mid -1;
+                
+            }
+            mid = start+ (end-start)/2;
+
+        }
+        return possible;
     }
 };
