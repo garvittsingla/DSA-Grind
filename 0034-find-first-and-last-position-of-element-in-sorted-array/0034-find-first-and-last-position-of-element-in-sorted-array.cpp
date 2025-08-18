7a8a8c5980e361ff3139ad270e595273fac3b149
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-08-18 14:53:21 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 18.3 MB (17.4%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    int startposition(vector<int> nums,int target){
+   int binarysearch(vector<int> nums,int target,bool isfirst){
         int start = 0;
         int end = nums.size()-1;
-        int mid = start + (end-start)/2;
         int possible = -1;
-        while(start <= end){
-           if(nums[mid]==target){
-            possible=mid;
-            end = mid-1;
-           }else if(nums[mid]>target){
-            end = mid-1;
-           }else{
-            start = mid+1;
-           }
-           mid = start + (end-start)/2;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(isfirst){
+                if(nums[mid]==target){
+                    possible = mid;
+                    end = mid-1;
+                }else if(nums[mid]>target){
+                    end= mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+            else{
+                 if(nums[mid]==target){
+                    possible = mid;
+                    start = mid+1;
+                }else if(nums[mid]>target){
+                    end= mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
         }
         return possible;
-    }
-
-     int endposition(vector<int> nums,int target){
-        int start = 0;
-        int end = nums.size()-1;
-        int mid = start + (end-start)/2;
-        int possible = -1;
-        while(start <= end){
-          if(nums[mid]==target){
-            possible=mid;
-           start = mid+1;
-           }else if(nums[mid]>target){
-            end = mid-1;
-           }else{
-            start = mid+1;
-           }
-           mid = start + (end-start)/2;
-        }
-        return possible;
-    }
-    
+   }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans;
-        ans.push_back(startposition(nums,target));
-        ans.push_back(endposition(nums,target));
+        ans.push_back(binarysearch(nums,target,true));
+        ans.push_back(binarysearch(nums,target,false));
         return ans;
     }
 };
