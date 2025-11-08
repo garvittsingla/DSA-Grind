6ec4f9ad7d735b5cc8b80ba712b3ca2aa1cf5a0c
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-11-08 18:59:01 +0530

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 11.7 MB (85.6%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int mini = 0;
+        int miniindex = 0;
         for(int i = 0 ; i < strs.size() ; i++){
-            if(strs[i].length() < strs[mini].length()){
-                mini = i;
+            if(strs[i].length() < strs[miniindex].length()){
+                miniindex = i;
             }
         }
-        string small = strs[mini];
-        while(small.length() > 0){
+        string possibleans = strs[miniindex];
+        while(possibleans.length()!=0){
             bool ispossible = true;
             for(auto &it:strs){
-                if(it.find(small)  != 0){
-                    ispossible = false;
+                if(it.find(possibleans)!=0){
+                    ispossible=false;
                 }
             }
-            if(ispossible) return small;
-            small.pop_back();
+
+            if (ispossible == true) break;
+            possibleans.pop_back();
+
         }
-        
-        return "";
-        
+        return possibleans;
     }
 };
