88cf54aa0ca278b04360c51e435214773e97b957
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-11-13 15:28:51 +0530

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 43.8 MB (5.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,38 +1,33 @@
 class Solution {
-    public int pivot(int[] nums){
-        int start = 0;
-        int end = nums.length -1 ;
-        while(start<end){
-            int mid = start + (end - start )/2;
-            if(nums[mid]>nums[end]){
-                start = mid+1;
-            }else{
-                end = mid;
-            }
-        }
-        return start;
-    }
-    public int binarysearch(int[] nums, int start, int end,int target){
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>target){
-                end = mid-1;
-            }else{
-                start = mid+1;
-            }
-        }
-        return -1;
-    }
     public int search(int[] nums, int target) {
-        int pivotindex = pivot(nums);
-        System.out.println(pivotindex);
-        int index = binarysearch(nums,0,pivotindex-1,target);
-        if(index!=-1) return index;
-        
-        return binarysearch(nums,pivotindex,nums.length-1,target);
-        
-        
+        int start = 0;
+        int end = nums.length - 1;
+        int n = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            boolean targetInUpper = target > nums[n];
+            boolean midInUpper = nums[mid] > nums[n];
+
+            if (targetInUpper == midInUpper) {
+                if (target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } 
+            else {
+                if (targetInUpper) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1;
     }
 }
