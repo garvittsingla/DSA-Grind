48037254c67795bf3154550d6a5316d6fbd2a0f0
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-11-28 13:44:04 +0530

    0090-SUBSETS-II Stats: Time: 3 ms (34.59%), Space: 11.4 MB (19.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    set<vector<int>> result;
-    void helper(int index, vector<int> &ds, vector<int> &nums){
-        if(index >= nums.size()){
-            result.insert(ds);
-            return;
+    vector<vector<int>> result;
+    void helper(int index, vector<int> ds, vector<int> &nums){
+        // if(index >= nums.size()){
+            result.push_back(ds);
+            // return;
+        // }
+        for(int i = index ; i < nums.size() ; i++){
+            if(i>index && nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.push_back(nums[i]);
+            helper(i+1,ds,nums);
+            ds.pop_back();
         }
-        ds.push_back(nums[index]);
-        helper(index+1,ds,nums);
-        ds.pop_back();
-        helper(index+1,ds,nums);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int> ds;
         helper(0,ds,nums);
-        vector<vector<int>> res = vector<vector<int>>(result.begin(),result.end());
-        return res;
+        return result;
     }
 };
