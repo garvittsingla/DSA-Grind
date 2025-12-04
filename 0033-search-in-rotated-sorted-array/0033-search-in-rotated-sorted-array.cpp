ba09de2c9893acc318e70a17d58583a6b2a388af
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-12-04 14:03:06 +0530

    0033-SEARCH-IN-ROTATED-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 15.3 MB (32.31%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,26 +3,25 @@ public:
     int search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size()-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
+        while(start <= end){
+            int mid = start + (end-start)/2;
             if(nums[mid]==target) return mid;
-            bool targetinupperhalf = target > nums[nums.size()-1];
-            bool midinupperhalf = nums[mid] > nums[nums.size()-1];
+            bool targetinupper = target > nums[nums.size()-1];
+            bool midinupper = nums[mid] > nums[nums.size()-1];
 
-            if(targetinupperhalf==midinupperhalf){
-                if(nums[mid]>target){
+            if(targetinupper==midinupper){
+                if(nums[mid] > target){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                };
+            }else{
+                if(targetinupper){
                     end = mid-1;
                 }else{
                     start = mid+1;
                 }
-            }else{
-                if(targetinupperhalf){
-                   end = mid-1;
-                }else{
-                    start = mid+1;
-                }
             }
-
         }
         return -1;
     }
