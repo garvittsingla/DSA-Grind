9382c61da38f5a44de5b795f7ade42a3e46ad844
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-12-09 11:03:01 +0530

    0084-LARGEST-RECTANGLE-IN-HISTOGRAM Stats: Time: 100 ms (31.52%), Space: 75.9 MB (59.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -44,11 +44,11 @@ class Solution {
         int[] nsl = nsl(heights);
         int maxarea = 0;
         for(int i = 0 ;i < heights.length ; i++){
-            int left = (nsl[i]  == -1 )? 0 : nsl[i] + 1;
-            int right = (nsr[i] == -1) ?  heights.length : nsr[i];
-            int width =  right - left ;
-            int area = width * heights[i];
-            maxarea = Math.max(maxarea,area);
+           int left = (nsl[i]==-1) ? 0 : nsl[i]+1;
+           int right = (nsr[i]==-1) ?heights.length : nsr[i];
+           int width= right-left;
+           int area = width * heights[i];
+           maxarea = Math.max(maxarea,area);
         }
         return maxarea;
     }
