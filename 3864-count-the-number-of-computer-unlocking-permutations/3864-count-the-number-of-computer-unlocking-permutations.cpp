0f4c1e65d7d2f922e25c600351e1ce41b2cc3623
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-12-10 16:30:45 +0530

    3864-COUNT-THE-NUMBER-OF-COMPUTER-UNLOCKING-PERMUTATIONS Stats: Time: 0 ms (100%), Space: 92.8 MB (47%) - LeetHub

--- 3864-count-the-number-of-computer-unlocking-permutations/3864-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/3864-count-the-number-of-computer-unlocking-permutations.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int countPermutations(vector<int>& complexity) {
-        long long count=1;
-        long long ans=1;
-        long long min1=complexity[0];
-        for(long long i=1;i<complexity.size();i++){
-            if(min1>=complexity[i]) return 0;
-            ans=(ans*count)%1000000007;
-            count++;
+        long long mod = 1000000007;
+        long long ans = 1;
+        for(int i = 1; i < complexity.size() ; i++){
+            if(complexity[i] <= complexity[0]) return 0;
+            ans = (ans*i)%mod;       
         }
-        return ans;
+            return (int)ans;
     }
 };
