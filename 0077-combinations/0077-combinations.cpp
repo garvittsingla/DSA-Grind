75cb933de45e0c4b709a7464f1e1ce5b7a4d4f7b
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2025-12-11 20:03:06 +0530

    0077-COMBINATIONS Stats: Time: 286 ms (6.82%), Space: 159.9 MB (5.85%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<vector<int>> result;
     void solve(int k,vector<int> ds,int n,int current){
-        if(ds.size()==k){
-            result.push_back(ds);
-            return;
-        }
-        for(int i = current ; i  <= n ;i++){
-            ds.push_back(i);
-            solve(k,ds,n,i+1);
-            ds.pop_back();
-        }
+       if(ds.size() == k){
+        result.push_back(ds);
+        return;
+       }
+       for(int i = current ; i <= n ;i++){
+        ds.push_back(i);
+        solve(k,ds,n,i+1);
+        ds.pop_back();
+       }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int> ds;
