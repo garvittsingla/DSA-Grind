672d7ed9f443e54460085b22216bbb1f9f891dac
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-01-12 14:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (62.02%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -18,10 +18,10 @@ public:
         while(even!=nullptr && even->next!=nullptr){
             current->next = even->next;
             current = current->next;
-            if(current) {
+            // if(current) {
                 even->next = current->next;
                 even = even->next;
-                };
+                // };
              
         }
         current->next = evenhead;
