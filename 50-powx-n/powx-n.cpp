2427b77c72a37f39d42c9054fdca819ea0acaf17
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-01-28 21:51:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.34%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,18 @@
-    class Solution {
-    public:
-        double power(double x , long n){
-            if(n == 0) return 1;
-            if(n < 0){
+class Solution {
+public:
+    double power(double x,long long n){
+        if(n == 0) return 1;
+        if(n<0){
             return power(1/x,-n);
-            }
-
-            if(n%2 == 0){
-                return power(x*x,n/2);
-            }
-            return x*power(x*x,(n-1)/2);
         }
-        double myPow(double x, int n) {
-            return power(x,(long)n);
-
+        if(n%2 == 0){
+            return power(x*x,n/2);
         }
-
-    };
+        return x*power(x*x,(n-1)/2);
+        
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        return power(x,N);
+    }
+};
