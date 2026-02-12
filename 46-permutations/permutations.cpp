59c14bfde0c85584ac475d94d5f3343cfa5f3d87
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-02-12 18:09:03 +0530

    Time: 3 ms (28.00%) | Memory: 10.9 MB (29.73%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void backtrack(vector<int> &nums,vector<int> &ds,set<int> &st){
-        if(ds.size() == nums.size()){
+    void backtrack(vector<int> &nums,vector<int> &ds,vector<bool> &taken){
+        if(nums.size() == ds.size()){
             ans.push_back(ds);
             return;
         }
-        if(ds.size() > nums.size()){
-            return;
-        }
+        if(nums.size() < ds.size()) return;
 
         for(int i = 0 ; i < nums.size() ; i++){
-            if(!st.count(nums[i])){
-                st.insert(nums[i]);
-                ds.push_back(nums[i]);
-                backtrack(nums,ds,st);
-                st.erase(nums[i]);
-                ds.pop_back();
-            }
+            if(taken[i] == true) continue;
+            taken[i] = true;
+            ds.push_back(nums[i]);
+            backtrack(nums,ds,taken);
+            taken[i] = false;
+            ds.pop_back();
         }
-    }
-    vector<vector<int>> permute(vector<int>& nums) {
-        set<int> st;
-        vector<int> ds;
-        backtrack(nums,ds,st);
-        return ans;
 
     }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> taken(nums.size(),false);
+        vector<int> ds;
+        backtrack(nums,ds,taken);
+        return ans;
+    }   
 };
