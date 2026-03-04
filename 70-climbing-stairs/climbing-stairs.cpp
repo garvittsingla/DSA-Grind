4ea6d7e324cae73ea5c9ae92cf336f3f64f84f0a
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-03-04 16:48:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
+    
     int backtrack(int n,int current,vector<int> &dp){
-        if(current == n){
-            return 1;
-        }
-        if(current > n) return 0;
-        
-        if(dp[current] == 0){
-            int one = backtrack(n,current+1,dp);
-            int two = backtrack(n,current+2,dp);
-            dp[current] = one+two;
-        }else{
-            return dp[current];
-        }
+       if(current == n) return 1;
+       if(current > n) return 0;
 
-        return dp[current];
+       if(dp[current] != -1) return dp[current];
+       int one =backtrack(n,current+1,dp);
+       int two =backtrack(n,current+2,dp);
+
+       dp[current] = one+two;
+       return dp[current];
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,0);
-        int ans = backtrack(n,0,dp);
-        return ans;
+        vector<int> dp(n+1,-1);
+        return backtrack(n,0,dp);
     }
 };
