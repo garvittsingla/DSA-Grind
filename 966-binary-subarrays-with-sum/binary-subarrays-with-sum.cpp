4723f4b1fca4d89e17eaf8b86d63a267e1265d66
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-03-09 18:02:33 +0530

    Time: 28 ms (9.16%) | Memory: 39.8 MB (8.79%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
-       vector<int> prefix(nums.size(),0);
-        int sum = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
-            sum+=nums[i];
-            prefix[i] = sum;
-        }
-        unordered_map<int,int> mpp;
-        int count = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(prefix[i] == k) count++;
-            int remaining = prefix[i] - k;
-            if(mpp.find(remaining) != mpp.end()){
-                count += mpp[remaining];
-            }
+      vector<int> prefix(nums.size(),-1);
+      int sum = 0;
+      for(int i = 0 ; i < nums.size() ; i++){
+        sum += nums[i];
+        prefix[i] = sum;
+      }
 
-            mpp[prefix[i]]++;
+      unordered_map<int,int> mpp;
+        int ans = 0;
+      for(int i = 0 ; i < nums.size() ; i++){
+        if(prefix[i] == k) ans++;
+        int rem =  prefix[i] - k;
+        if(mpp.count(rem)){
+            ans += mpp[rem];
         }
-        return count;
+        mpp[prefix[i]]++;
+      }
+      return ans;
     }
 };
