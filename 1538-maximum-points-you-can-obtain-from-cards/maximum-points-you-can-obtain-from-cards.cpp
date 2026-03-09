12b4424609322eb3fd00c330d2afd20f5e5a92da
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-03-09 11:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (93.40%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums, int k) {
-        //what i can do is take total of first n cards and mkae them in ans, then atake a card from it and add a card from the end , so we can make it
         int total = 0;
         for(int i = 0 ; i < k ; i++){
             total += nums[i];
         }
         int ans = total;
-        for(int i = 0; i< k ; i++){
+
+        for(int i = 0 ; i < k ; i++){
             total -= nums[k-i-1];
-            total += nums[nums.size()-1-i];
+            total += nums[nums.size() - i - 1];
             ans = max(ans,total);
         }
         return ans;
