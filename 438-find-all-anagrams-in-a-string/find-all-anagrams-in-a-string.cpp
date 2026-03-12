06700124b612dec9331a1ec68dd8dc58aa8bdd3f
Garvit Singla <159273915+garvittsingla@users.noreply.github.com>
2026-03-12 18:49:02 +0530

    Time: 11 ms (31.45%) | Memory: 11.4 MB (83.99%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        if(s.size()<p.size()) return {};
+        vector<int> freqa(26,0);
+        for(int i= 0 ; i < p.length() ; i++){
+            freqa[p[i]-'a']++;
+        }
+        vector<int> ans;
+        int k = p.size();
+        vector<int> freqb(26,0);
+        for(int i = 0 ; i < k ; i++){
+            freqb[s[i]-'a']++;
+        }
+        if(freqa==freqb) ans.push_back(0);
+        for(int i = k ; i < s.size() ; i++){
+            freqb[s[i]-'a']++;
+            freqb[s[i-k]-'a']--;
+
+            if(freqa == freqb) ans.push_back(i-k+1);
+
+        }
+        return ans;
+
+    }
+};
